cbca66a771d922e3a47650267f1c53c8334a6460
lmc2179 <louiscialdella@gmail.com>
2018-11-18 13:33:52 -0600

    re-added gower folder

---
+++ gower/measure.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# TODO: Support for list of lists, panda dataframes'
+# TODO: Type validation
+
+R = 'R'
+B = 'B'
+C = 'C'
+
+def _tile_column(col):
+    r = np.vstack([col for _ in range(len(col))])
+    return r.T, r
+
+def similarity(X, types):
+    s_matrix = np.zeros((len(X), len(X)))
+    d_matrix = np.zeros((len(X), len(X)))
+    for c, t in enumerate(types):
+        col_vector = X[:,c]
+        col_matrix, row_matrix = _tile_column(col_vector)
+        if t == R:
+            r_k = max(col_vector) - min(col_vector)
+            s_matrix = s_matrix + (1.0 - (1.*np.abs(col_matrix - row_matrix) / r_k))
+            d_matrix = d_matrix + np.ones(d_matrix.shape)
+        elif t == B:
+            s_matrix = s_matrix + ((col_matrix + row_matrix) == 2)
+            d_matrix = d_matrix + (1. - ((col_matrix + row_matrix) == 0))
+        elif t == C:
+            s_matrix = s_matrix + (col_matrix == row_matrix)
+            d_matrix = d_matrix + np.ones(d_matrix.shape)
+        else: 
+            raise Exception('unrecognized type')
+    return s_matrix / d_matrix
+
+def distance(X, types):
+    return 1.0 - similarity(X, types)
---
+++ gower/tests/test_measure.py
@@ -0,0 +1,52 @@
+from gower.measure import similarity, distance
+import unittest
+import numpy as np
+
+class GowerTest(unittest.TestCase):
+    def test_real(self):
+        X = np.array([[0],
+                      [1],
+                      [2]])
+        T = ['R']
+        S = similarity(X, T)
+        expected_sim = [[1, 0.5, 0],
+                        [0.5, 1, 0.5],
+                        [0, 0.5, 1]]
+        self.assertEqual(S.tolist(), expected_sim)
+
+    def test_categorical(self):
+        X = np.array([[0],
+                      [1],
+                      [2]])
+        T = ['C']
+        S = similarity(X, T)
+        expected_sim = [[1, 0, 0],
+                        [0, 1, 0],
+                        [0, 0, 1]]
+        self.assertEqual(S.tolist(), expected_sim)
+        
+    def test_gower_similarity(self):
+        X = np.array([[0, 0, 1],
+                      [1, 1, 1],
+                      [2, 0, 2]])
+        T = ['R', 'B', 'C']
+        S = similarity(X, T)
+        expected_sim = [[1, 1./2, 0],
+                        [1./2, 1, 1./6],
+                        [0, 1./6, 1]]
+        self.assertEqual(S.tolist(), expected_sim)
+        
+    def test_gower_distance(self):
+        X = np.array([[0, 0, 1],
+                      [1, 1, 1],
+                      [2, 0, 2]])
+        T = ['R', 'B', 'C']
+        D = distance(X, T)
+        expected_dis = [[0, 1./2, 1.],
+                        [1./2, 0, 5./6],
+                        [1., 5./6, 0]]
+        self.assertEqual(D.tolist(), expected_dis)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
